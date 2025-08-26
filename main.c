adad86d3c9968ec1b0ccca7f10056bf06e67aeae
grant-tm <grant.t.morgan@gmail.com>
2025-08-26 14:53:32 -0500

    create basic sdl window

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 #define XTDLIB_IMPLEMENTATION
 #include "xtdlib/xtdlib.h"
 
-int main (void) {
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_opengl.h>
+
+i32 main(void) {
+    
+	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_EVENTS);
+	SDL_Window *window = SDL_CreateWindow("SDL3 OpenGL Test", 960, 540, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+	SDL_GLContext glctx = SDL_GL_CreateContext(window);
+
+    bool program_running = true;
+    while (program_running) {
+        
+		SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_QUIT) {
+                program_running = false;
+            }
+        }
+
+        glClearColor(.1f, .1f, .1f, .1f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        SDL_GL_SwapWindow(window);
+    }
+
+    SDL_GL_DestroyContext(glctx);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    
 	return 0;
 }
+
