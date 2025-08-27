eb01797457e276aab95fe7021400b35f46216ba9
grant-tm <grant.t.morgan@gmail.com>
2025-08-27 11:40:17 -0500

    refactor application state and sdl event handler

--- main.c
+++ main.c
@@ -8,8 +8,6 @@
 #include "clay.h"
 #include "renderers/SDL3/clay_renderer_SDL3.c"
 
-
-
 void clay_error_handler(Clay_ErrorData errorData) {
     printf("%s", errorData.errorText.chars);
     //switch(errorData.errorType) {}
@@ -27,76 +25,92 @@ typedef struct MouseState {
 	bool is_down;
 } MouseState;
 
+typedef struct ApplicationState {
+	bool running;
+	SDL_Window *window;
+	SDL_GLContext gl_context;
+	Clay_SDL3RendererData renderer_data;
+	Clay_Arena clay_arena;
+	MouseState mouse_state;
+} ApplicationState;
+
+void sdl_event_handler (ApplicationState *app, SDL_Event *event) {
+	switch (event->type) {
+		case SDL_EVENT_QUIT:
+		{
+			app->running = false;
+			break;
+		}
+		case SDL_EVENT_MOUSE_MOTION:
+		{
+			app->mouse_state.position_x = event->motion.x;
+			app->mouse_state.position_y = event->motion.y;
+			break;
+		}
+		case SDL_EVENT_MOUSE_WHEEL:
+		{
+			app->mouse_state.wheel_x = event->wheel.x;
+			app->mouse_state.wheel_y = event->wheel.y;
+			break;
+		}
+		case SDL_EVENT_MOUSE_BUTTON_DOWN:
+		{
+			app->mouse_state.is_down = true;
+			break;
+		}
+		case SDL_EVENT_MOUSE_BUTTON_UP:
+		{
+			app->mouse_state.is_down = false;
+			break;
+		}
+	}
+}
+
 i32 main(i32 argc, char *argv[]) {
 	xtd_ignore_unused(argc);
 	xtd_ignore_unused(argv);
 
+	ApplicationState application_state;
+
 	// Initialize SDL
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_EVENTS);
-	Clay_SDL3RendererData renderer_data;
-	SDL_Window *window;
 	SDL_CreateWindowAndRenderer(
         "SDL3 OpenGL Test",
         960, 540,
         SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE,
-		&window,
-		&renderer_data.renderer
+		&application_state.window,
+		&application_state.renderer_data.renderer
     );
-    SDL_SetWindowTitle(window, "IQ");
-    SDL_GLContext glctx = SDL_GL_CreateContext(window);
+    SDL_SetWindowTitle(application_state.window, "IQ");
+    application_state.gl_context = SDL_GL_CreateContext(application_state.window);
 
 	// Initialize Clay
 	u64 clay_memory_size = Clay_MinMemorySize();
-    Clay_Arena arena = Clay_CreateArenaWithCapacityAndMemory(clay_memory_size, malloc(clay_memory_size));
-    Clay_Initialize(arena, (Clay_Dimensions) { 960, 540 }, (Clay_ErrorHandler) { clay_error_handler, 0 });
+	application_state.clay_arena = Clay_CreateArenaWithCapacityAndMemory(clay_memory_size, malloc(clay_memory_size));
+    Clay_Initialize(application_state.clay_arena, (Clay_Dimensions) { 960, 540 }, (Clay_ErrorHandler) { clay_error_handler, 0 });
 	
 	// Initialize application state
-	MouseState mouse_state = {0};
+	//application_state.mouse_state = {0};
 
-    bool program_running = true;
-    while (program_running) {
-        SDL_Event e;
-
-		// Handle Events
-        while (SDL_PollEvent(&e)) {
-            switch (e.type) {
-				case SDL_EVENT_QUIT:
-				{
-					program_running = false;
-					break;
-				}
-				case SDL_EVENT_MOUSE_MOTION:
-				{
-					mouse_state.position_x = e.motion.x;
-					mouse_state.position_y = e.motion.y;
-					break;
-				}
-				case SDL_EVENT_MOUSE_WHEEL:
-				{
-					mouse_state.wheel_x = e.wheel.x;
-					mouse_state.wheel_y = e.wheel.y;
-					break;
-				}
-				case SDL_EVENT_MOUSE_BUTTON_DOWN:
-				{
-					mouse_state.is_down = true;
-					break;
-				}
-				case SDL_EVENT_MOUSE_BUTTON_UP:
-				{
-					mouse_state.is_down = false;
-					break;
-				}
-			}
+    application_state.running = true;
+    while (application_state.running) {
+		
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+        	sdl_event_handler(&application_state, &event);    
         }
 		
 		i32 screen_width, screen_height;
-		SDL_GetWindowSize(window, &screen_width, &screen_height);
+		SDL_GetWindowSize(application_state.window, &screen_width, &screen_height);
         Clay_SetLayoutDimensions((Clay_Dimensions) { screen_width, screen_height });
+        Clay_SetPointerState((Clay_Vector2) { 
+				application_state.mouse_state.position_x, 
+				application_state.mouse_state.position_y }, 
+				application_state.mouse_state.is_down);
+		Clay_UpdateScrollContainers(true, (Clay_Vector2) { 
+				application_state.mouse_state.wheel_x, application_state.mouse_state.wheel_y }, 0.1);
         
-		Clay_SetPointerState((Clay_Vector2) { mouse_state.position_x, mouse_state.position_y }, mouse_state.is_down);
-		Clay_UpdateScrollContainers(true, (Clay_Vector2) { mouse_state.wheel_x, mouse_state.wheel_y }, 0.1);
-        Clay_BeginLayout(); 
+		Clay_BeginLayout(); 
         CLAY({ 	
 			.id = CLAY_ID("OuterContainer"), 
 			.layout = { .sizing = {CLAY_SIZING_GROW(0), CLAY_SIZING_GROW(0)}, 
@@ -120,15 +134,15 @@ i32 main(i32 argc, char *argv[]) {
 		}
 		Clay_RenderCommandArray render_commands = Clay_EndLayout();
     	
-		SDL_SetRenderDrawColor(renderer_data.renderer, 0, 0, 0, 255);
-    	SDL_RenderClear(renderer_data.renderer);
-		SDL_Clay_RenderClayCommands(&renderer_data, &render_commands);
-		SDL_RenderPresent(renderer_data.renderer);
+		SDL_SetRenderDrawColor(application_state.renderer_data.renderer, 0, 0, 0, 255);
+    	SDL_RenderClear(application_state.renderer_data.renderer);
+		SDL_Clay_RenderClayCommands(&application_state.renderer_data, &render_commands);
+		SDL_RenderPresent(application_state.renderer_data.renderer);
 	}
 
     // Cleanup
-    SDL_GL_DestroyContext(glctx);
-    SDL_DestroyWindow(window);
+    SDL_GL_DestroyContext(application_state.gl_context);
+    SDL_DestroyWindow(application_state.window);
     SDL_Quit();
 
     return 0;
